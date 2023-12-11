1e8a11ddf832b291f993676af93cbe5b68e94cd9
git2 <mahmudulhasanm56@gmail.com>
2023-12-11 21:08:14 +0600

    ecomerch

---
+++ index.js
@@ -0,0 +1,10 @@
+let topSalesBox = document.querySelector(".top-sales-box");
+let upperScndBtn = document.querySelector(".upper-scnd-btn");
+btn2 = upperScndBtn.querySelector(".btn-2"),
+btn3 = upperScndBtn.querySelector(".btn-3"),
+btn4 = upperScndBtn.querySelector(".btn-4");
+
+btn2.onclick = ()=>{
+    top-sales-box.classlist.add("activeinfo");
+    home-add.classlist.remove("activeinfo");
+}
---
+++ test2.js
@@ -0,0 +1,4 @@
+function scrollToDiv() {
+    var div2 = document.getElementById('div2');
+    div2.scrollIntoView({ behavior: 'smooth' });
+  }
